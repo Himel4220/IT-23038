833d69bd7886ff5a0138cc9adbefcd95a98abf7f
Himel4220 <arafathossain4499@gmail.com>
2024-11-18 13:06:10 +0600

    Add files via upload

---
+++ Chapter 1/Ch1Ex4.java
@@ -0,0 +1,5 @@
+public class Ch1Ex4 {
+public static main(String[] args) { //Error in this line void is missing
+System.out.println("Welcome to Java);//In this line double quotes is missing
+}
+}
---
+++ Chapter 1/Ch1Ex5.java
@@ -0,0 +1,7 @@
+/*This is a sample code of runtime errors.Here 1/0 expression will cause infinity*/
+
+public class Ch1Ex5 {
+public static void main(String[] args) {
+System.out.println(1 / 0);
+}
+}
