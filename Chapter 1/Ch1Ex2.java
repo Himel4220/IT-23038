a8781417f4c7d8878aeaa1fe22f33b46e661fd48
Himel4220 <arafathossain4499@gmail.com>
2024-11-18 12:00:08 +0600

    Add files via upload

---
+++ Chapter 1/Ch1Ex2.java
@@ -0,0 +1,9 @@
+/*This is a java program that prints three messages to the console*/
+
+public class Ch1Ex2 {
+public static void main(String[] args) {
+System.out.println("Programming is fun!");
+System.out.println("Fundamentals First");
+System.out.println("Problem Driven");
+}
+}
